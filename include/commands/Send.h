f40751a5b8d63c5b7b5468f3434f8f6fd4ff47fe
Mark <pal.mark28@gmail.com>
2019-06-23 18:19:15 +0200

    add override specifier

--- include/commands/Send.h
+++ include/commands/Send.h
@@ -15,8 +15,8 @@ namespace Commands {
 
     public:
         SendCmd(const std::shared_ptr<SerialManager>& _manager);
-        void execute(const std::string&);
-        Command* clone() const;     //not rly needed
+        void execute(const std::string&) override;
+        Command* clone() const override;     //not rly needed
     };
 
 }   
