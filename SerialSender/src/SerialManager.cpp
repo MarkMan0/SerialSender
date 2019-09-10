8dc64df48beeefa2d124149a9ed3b1e2ebea925a
Mark <pal.mark28@gmail.com>
2019-09-10 11:00:00 +0200

    Overlapped reading of event, in synchronous fashion

--- SerialSender/src/SerialManager.cpp
+++ SerialSender/src/SerialManager.cpp
@@ -11,56 +11,56 @@
 
 SerialManager::SerialManager(const std::string& name, const long& baud) {
 
-    //defines how much to wait for a message
-    COMMTIMEOUTS timeout = { 0 };
-    timeout.ReadIntervalTimeout = MAXDWORD ;
-    timeout.ReadTotalTimeoutConstant = 0;
-    timeout.ReadTotalTimeoutMultiplier = 0 ;
-    timeout.WriteTotalTimeoutConstant = 0;
-    timeout.WriteTotalTimeoutMultiplier = 0;
+	//defines how much to wait for a message
+	COMMTIMEOUTS timeout = { 0 };
+	timeout.ReadIntervalTimeout = MAXDWORD;
+	timeout.ReadTotalTimeoutConstant = 0;
+	timeout.ReadTotalTimeoutMultiplier = 0;
+	timeout.WriteTotalTimeoutConstant = 0;
+	timeout.WriteTotalTimeoutMultiplier = 0;
 
-    port.open(name, baud, timeout);   //open the port
+	port.open(name, baud, timeout);   //open the port
 
-    open = true;    //TODO: assuming everything works
+	open = true;    //TODO: assuming everything works
 }
 
 
 void SerialManager::readPort() {
-    if(!isOpen()) return;       //port no open, nothing to do
+	if (!isOpen()) return;       //port no open, nothing to do
 
-    portMtx.lock(); //TODO:: called periodically, need to wait???
-    std::string msg = port.receive();
-    if(msg.size() > 1)
-        msgCont.push_back_mtx(msg);  //read a message and push to the queue
+	portMtx.lock(); //TODO:: called periodically, need to wait???
+	std::string msg = "";
+	if (msg.size() > 1)
+		msgCont.push_back_mtx(msg);  //read a message and push to the queue
 
-    portMtx.unlock();       //unclock the mutex
+	portMtx.unlock();       //unclock the mutex
 }
 
 
 void SerialManager::closePort() {
-    port.close();
+	port.close();
 }
 
 std::string SerialManager::lastMsg() {
-    std::string msg;
-    msgCont.pop_back_to(msg);
-    return msg;
+	std::string msg;
+	msgCont.pop_back_to(msg);
+	return msg;
 }
 
 std::string SerialManager::nextMsg() {
-    std::string tmp;
-    msgCont.pop_front_to(tmp);     //pops and returns the next message from the queue
-    return tmp;
+	std::string tmp;
+	msgCont.pop_front_to(tmp);     //pops and returns the next message from the queue
+	return tmp;
 }
 
 //sends a message when the port mutex is free
 void SerialManager::writeMsg(const std::string& msg) {
-    if(!isOpen()) return;
-    portMtx.lock();
+	if (!isOpen()) return;
+	portMtx.lock();
 
-    port.send(msg);
+	port.send(msg);
 
-    portMtx.unlock();
+	portMtx.unlock();
 }
 
 void SerialManager::readThread(unsigned long ms) {
@@ -70,16 +70,68 @@ void SerialManager::readThread(unsigned long ms) {
 	char  chRead;
 
 
-	while(1) {
+	OVERLAPPED osReader = { 0 };
+	BOOL fWaitingOnRead = FALSE;
+	osReader.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+
+	while (1) {
 		if (WaitCommEvent(port.hSerial, &dwCommEvent, NULL)) {
 			//receive char event occoured
+			//reead all the available bytes
 			do {
-				if (ReadFile(port.hSerial, &chRead, 1, &dwRead, NULL)) {
-					// A byte has been read; process it.
+				//begin overlapped reading
+				//returns TRUE if reading done immediately, false if error OR waiting
+				if (!ReadFile(port.hSerial, port.buff, port.buffSz, &dwRead, &osReader)) {
+					if (GetLastError() != ERROR_IO_PENDING) {   // read not delayed?
+						//Error occoured
+						//break;
+					}
+					else {
+						//read not done immediately, need to wait
+						fWaitingOnRead = TRUE;
+					}
 				}
 				else {
-					// An error occurred in the ReadFile call.
-					break;
+					// read completed immediately
+					fWaitingOnRead = FALSE;
+					std::cout << port.buff;
+				}
+				DWORD dwRes;	//the result of the wait
+
+				if (fWaitingOnRead) {		//if we are waiting on a read
+					dwRes = WaitForSingleObject(osReader.hEvent, INFINITE);		//wait forever
+					switch (dwRes) {
+						// Read completed.
+					case WAIT_OBJECT_0:
+						if (!GetOverlappedResult(port.hSerial, &osReader, &dwRead, FALSE)) {
+							// Error in communications; report it.
+							//break;
+						}
+						else {
+							// Read completed successfully.
+							std::cout << port.buff;
+						}
+
+						//  Reset flag so that another opertion can be issued.
+						fWaitingOnRead = FALSE;
+						break;
+
+					case WAIT_TIMEOUT:
+						5 + 5;
+						// Operation isn't complete yet. fWaitingOnRead flag isn't
+						// changed since I'll loop back around, and I don't want
+						// to issue another read until the first one finishes.
+						//
+						// This is a good time to do some background work.
+						//break;
+
+					default:
+						5 + 5;
+						// Error in the WaitForSingleObject; abort.
+						// This indicates a problem with the OVERLAPPED structure's
+						// event handle.
+						//break;
+					}
 				}
 			} while (dwRead);	//while the bytes read is greater than 0, read
 		}
@@ -93,6 +145,6 @@ void SerialManager::readThread(unsigned long ms) {
 
 
 void SerialManager::startPeriodicRead(unsigned long ms) {
-    t = std::thread(&SerialManager::readThread, this, ms);
-    //t.join();
+	t = std::thread(&SerialManager::readThread, this, ms);
+	//t.join();
 }
