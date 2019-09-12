275b5a5d0bbccfdfbe2297ea46416202e22c6357
Mark <pal.mark28@gmail.com>
2019-09-12 22:57:51 +0200

    refactoring

--- SerialSender/src/SerialPort.cpp
+++ SerialSender/src/SerialPort.cpp
@@ -141,92 +141,103 @@ void SerialPort::send(const std::string& cmd) {
 }
 
 
+inline void readAvailable(HANDLE* h, std::string& dest) {
+
+	char chRead = '\0';
+
+	DWORD dwRead = 0;
+
+	OVERLAPPED ov = { 0 };
+
+	bool handleOpen = false;
+
+	auto eventCloser = [&handleOpen](OVERLAPPED* ov) {	if(handleOpen) CloseHandle(ov->hEvent); };
+
+	std::unique_ptr<OVERLAPPED, decltype(eventCloser)> osReader(&ov, eventCloser);
+
+	osReader->hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+	if (osReader->hEvent == NULL) {
+		return;
+	}
+	else {
+		handleOpen = true;
+	}
+
+	BOOL fWaitingOnRead = FALSE;
+	//receive char event occoured
+	//reead all the available bytes
+	do {
+		//begin overlapped reading
+		//returns TRUE if reading done immediately, false if error OR waiting
+		if (!ReadFile(*h, &chRead, 1, &dwRead, osReader.get())) {
+			if (GetLastError() != ERROR_IO_PENDING) {   // read not delayed?
+				//Error occoured
+				break;
+			}
+			else {
+				//read not done immediately, need to wait
+				fWaitingOnRead = TRUE;
+			}
+		}
+		else {
+			// read completed immediately
+			fWaitingOnRead = FALSE;
+			dest += chRead;
+		}
+		DWORD dwRes;	//the result of the wait
+
+		if (fWaitingOnRead) {		//if we are waiting on a read
+			dwRes = WaitForSingleObject(osReader->hEvent, INFINITE);		//wait forever
+			switch (dwRes) {
+				// Read completed.
+			case WAIT_OBJECT_0:
+				if (!GetOverlappedResult(*h, osReader.get(), &dwRead, FALSE)) {
+					// Error in communications; report it.
+					//break;
+				}
+				else {
+					// Read completed successfully.
+					dest += chRead;
+				}
+
+				//  Reset flag so that another opertion can be issued.
+				fWaitingOnRead = FALSE;
+				break;
+
+			case WAIT_TIMEOUT:
+				//TODO:
+				//	count the timeouts, and decide, whether the com port was disconnected
+
+				// Operation isn't complete yet. fWaitingOnRead flag isn't
+				// changed since I'll loop back around, and I don't want
+				// to issue another read until the first one finishes.
+				//
+				// This is a good time to do some background work.
+				break;
+
+			default:
+				// Error in the WaitForSingleObject; abort.
+				// This indicates a problem with the OVERLAPPED structure's
+				// event handle.
+				break;
+			}
+		}
+	} while (dwRead);	//while the bytes read is greater than 0, read
+}
+
+
+
 
 //on receive event, reads the port while there is data available, and then returns
 //the whole data as std::string
 std::string SerialPort::readOnEvent() {
-
 	DWORD dwCommEvent = 0;
-	DWORD dwRead = 0;
-	char  chRead = 0;
 
 	std::string msg("");
 
-	OVERLAPPED ov = { 0 };
-
-	auto eventCloser = [](OVERLAPPED* ov) {	CloseHandle(ov->hEvent); };
-
-	std::unique_ptr<OVERLAPPED, decltype(eventCloser)> osReader(&ov, eventCloser);
-
-
-
-	BOOL fWaitingOnRead = FALSE;
-	osReader->hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
-	if (osReader->hEvent == NULL) {
-		return msg;
-	}
-
+	
 	if (WaitCommEvent(hSerial, &dwCommEvent, NULL)) {
-		fWaitingOnRead = FALSE;
-		//receive char event occoured
-		//reead all the available bytes
-		do {
-			//begin overlapped reading
-			//returns TRUE if reading done immediately, false if error OR waiting
-			if (!ReadFile(hSerial, &chRead, 1, &dwRead, osReader.get())) {
-				if (GetLastError() != ERROR_IO_PENDING) {   // read not delayed?
-					//Error occoured
-					break;
-				}
-				else {
-					//read not done immediately, need to wait
-					fWaitingOnRead = TRUE;
-				}
-			}
-			else {
-				// read completed immediately
-				fWaitingOnRead = FALSE;
-				msg += chRead;
-			}
-			DWORD dwRes;	//the result of the wait
-
-			if (fWaitingOnRead) {		//if we are waiting on a read
-				dwRes = WaitForSingleObject(osReader->hEvent, INFINITE);		//wait forever
-				switch (dwRes) {
-					// Read completed.
-				case WAIT_OBJECT_0:
-					if (!GetOverlappedResult(hSerial, osReader.get(), &dwRead, FALSE)) {
-						// Error in communications; report it.
-						//break;
-					}
-					else {
-						// Read completed successfully.
-						msg += chRead;
-					}
-
-					//  Reset flag so that another opertion can be issued.
-					fWaitingOnRead = FALSE;
-					break;
-
-				case WAIT_TIMEOUT:
-					//TODO:
-					//	count the timeouts, and decide, whether the com port was disconnected
-
-					// Operation isn't complete yet. fWaitingOnRead flag isn't
-					// changed since I'll loop back around, and I don't want
-					// to issue another read until the first one finishes.
-					//
-					// This is a good time to do some background work.
-					break;
-
-				default:
-					// Error in the WaitForSingleObject; abort.
-					// This indicates a problem with the OVERLAPPED structure's
-					// event handle.
-					break;
-				}
-			}
-		} while (dwRead);	//while the bytes read is greater than 0, read
+		readAvailable(&hSerial, msg);
 	}
 	else {
 		// Error in WaitCommEvent
